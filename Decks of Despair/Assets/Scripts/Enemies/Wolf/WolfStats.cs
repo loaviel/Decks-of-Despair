cc4c1e75324eba45a7b1960011f3e265d4ab0249
loaviel <22408925@bucks.ac.uk>
2024-12-05 00:43:04 +0000

    edited animation controllers
    -- set up animation controllers / animators
    
    -- base code to call animation on enemy death, needs to be worked on as it breaks the whole game currently -- commented out

--- Decks of Despair/Assets/Scripts/Enemies/Wolf/WolfStats.cs
+++ Decks of Despair/Assets/Scripts/Enemies/Wolf/WolfStats.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class SquareEnemyStats : EnemyStats
 {
+
+   // public Animation animation;
     new void Start()
     {
+      //  animation = GetComponent<Animation>();
+
         base.Start();
        
         // Unique stats for wolf enemy -- overriden from base EnemyStats
@@ -19,7 +23,7 @@ public class SquareEnemyStats : EnemyStats
     {
         // Play the relevant death noise
         audioManager.WolfPlay();
-
+      //  animation.Play();
         base.Die();
     }
 }
